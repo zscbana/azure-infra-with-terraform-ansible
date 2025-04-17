9aa96a2ac7a097fea800438baf022739ac5783c3
zscbana <zscbana@gmail.com>
2025-04-17 22:50:40 +0200

    feat(network): add bidirectional VNet peering between West Europe, France, and East US to enable cross-region VM communication for Ansible configuration

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -30,3 +30,55 @@ resource "azurerm_virtual_network" "westEurope01Vnet" {
     Project = "terraform_ansible"
   }
 }
+
+# West Euro to france peering and france to west euro
+
+resource "azurerm_virtual_network_peering" "peering_WestEurope2France" {
+  name                         = "peering_WestEurope2France"
+  resource_group_name          = var.hubWestEurope01RGName
+  virtual_network_name         = azurerm_virtual_network.westEurope01Vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.france01Vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+  allow_gateway_transit = false
+}
+
+resource "azurerm_virtual_network_peering" "peering_France2WestEurope" {
+  name                         = "peering_France2WestEurope"
+  resource_group_name          = var.france01RGName
+  virtual_network_name         = azurerm_virtual_network.france01Vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.westEurope01Vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+  allow_gateway_transit = false
+}
+
+# West Euro Hub to East US peering
+
+resource "azurerm_virtual_network_peering" "peering_WestEurope2EastUS" {
+  name                         = "peering_WestEurope2EastUS"
+  resource_group_name          = var.hubWestEurope01RGName
+  virtual_network_name         = azurerm_virtual_network.westEurope01Vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.eastUS01Vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+  allow_gateway_transit = false
+}
+
+resource "azurerm_virtual_network_peering" "peering_EastUS2WestEurope" {
+  name                         = "peering_EastUS2WestEurope"
+  resource_group_name          = var.eastUS01RGName
+  virtual_network_name         = azurerm_virtual_network.eastUS01Vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.westEurope01Vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+  allow_gateway_transit = false
+}
